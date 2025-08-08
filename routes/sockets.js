995fe251db026bcadeeba978608a2c56e1456eb6
Stephen Moss <bladexr8@gmail.com>
2025-08-08 20:35:14 +1000

    attempt to fix nickname issue

--- routes/sockets.js
+++ routes/sockets.js
@@ -3,11 +3,21 @@ var socketIo = require("socket.io");
 exports.initialize = function (server) {
   io = socketIo(server);
 
+  // Shared storage for user data
+  const userSessions = new Map();
+
   const chatInfra = io.of("/chat_infra").on("connection", function (socket) {
+    console.log(`Connected to chatInfra channel on socket ${socket.id}`);
     socket.on("set_name", function (data) {
       console.log("Received set_name Event...");
 
       // Store nickname directly on socket
+      userSessions.set(socket.id, {
+        nickname: data.name,
+        joinTime: new Date(),
+      });
+
+      // Also store on this socket for this namespace
       socket.data.nickname = data.name;
 
       // emit confirmation
@@ -21,17 +31,23 @@ exports.initialize = function (server) {
 
       // broadcase user entry into chat
       socket.broadcast.emit("user_entered", data);
+
+      // clean up on disconnect
+      socket.on("disconnect", () => {
+        userSessions.delete(socket.id);
+      });
     });
   });
 
   const chatCom = io.of("/chat_com").on("connection", function (socket) {
+    console.log(`Connected to chatCom channel on socket ${socket.id}`);
     socket.on("message", function (message) {
       console.log("Receiving Message on chatCom Channel...");
       //message = JSON.parse(message);
       if (message.type === "userMessage") {
-        // Get nickname directly from socket data
-        const nickname = socket.data.nickname;
-        message.username = nickname;
+        // Get nickname from shared storage
+        const userData = userSessions.get(socket.id);
+        const nickname = userData ? userData.nickname : "Anonymous";
 
         const clientMessage = {
           type: message.type,
@@ -48,9 +64,15 @@ exports.initialize = function (server) {
         socket.broadcast.emit("message", clientMessage);
 
         // Send to connecting client (sender) with modified type
-        message.type = "myMessage";
-        socket.emit("message", clientMessage);
+        socket.emit("message", {
+          ...clientMessage,
+          type: "myMessage",
+        });
       }
+    });
+    // clean up on disconnect
+    socket.on("disconnect", () => {
+      userSessions.delete(socket.id);
     });
   });
 };
